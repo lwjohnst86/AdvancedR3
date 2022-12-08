50e17f40dfa6d4b1ca6386bd32d069d4600f75e7
Anders Askeland <anders.askeland@gmail.com>
2022-12-08 10:48:20 +0100

    more stuff

--- R/functions.R
+++ R/functions.R
@@ -8,5 +8,7 @@ descriptive_stats <- function(data) {
   data %>%
     dplyr::group_by(metabolite) %>%
     dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
-    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), round, digits = 1))
+    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), round,
+      digits = 1
+    ))
 }
