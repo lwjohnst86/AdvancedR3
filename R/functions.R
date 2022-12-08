d864c7bb82e10f0351180ce7b482d64c1bf7f384
Anders Askeland <anders.askeland@gmail.com>
2022-12-08 10:46:54 +0100

    Stuff

---
+++ R/functions.R
@@ -0,0 +1,12 @@
+#' Calculate descriptive statistics of each metabolite.
+#'
+#' @param data Lipidomics dataset.
+#'
+#' @return A data.frame/tibble.
+#'
+descriptive_stats <- function(data) {
+  data %>%
+    dplyr::group_by(metabolite) %>%
+    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
+    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), round, digits = 1))
+}
